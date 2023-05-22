d56fb46e135bb91b33f78888eb3b7a326413a6da
sam20B <wafulabibiana93@gmail>
2023-05-22 13:10:13 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = '0'; alph <= '9' ; alph++)
+	putchar(alph);
+	putchar('\n');
+	return (0);
+}
